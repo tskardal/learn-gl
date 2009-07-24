212af6f2366f4a6d912ea6fdbafafc872e2ed4ef
Thomas Skardal aka l3dx <thomas.skardal@gmail.com>
2009-07-24 13:43:28 +0200

    Now possible to quit the program with ESC or closing the window.

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,52 @@
 
 using namespace LearnGL;
 
+bool processEvents();
+
 int main(int argc, char** argv)
 {
     Window* wnd = new SDLWindow();
     wnd->create(800,600,32,false,"Test");
-    
+
     while(wnd->isRunning()) 
     {
+	if(!processEvents())
+	{
+	    break;
+
+	}
 	wnd->nextFrame();
     }
 
     delete wnd;
 
     std::cout << "lala :)" << std::endl;
+}
+
+// todo: this should be abstracted into its own class
+//       perhaps "Application"
+bool processEvents()
+{
+    SDL_Event event;
+    
+    while(SDL_PollEvent(&event))
+    {
+	switch(event.type)
+	{
+	case SDL_QUIT:
+	{
+	    return false;
+	}
+	case SDL_KEYDOWN:
+	{
+	    SDLKey sym = event.key.keysym.sym;
+
+	    if(sym == SDLK_ESCAPE)
+	    {
+		return false;
+	    }
+	}
+	}
+    }
+    return true;
 }
